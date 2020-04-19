50c4b0fc5dee88b162f8305a6469e24b0c3db95c
TryingHard <44312638+GarlicCS@users.noreply.github.com>
2020-04-19 23:59:52 +0700

    Update config.js

--- database/Firebase/config.js
+++ database/Firebase/config.js
@@ -2,14 +2,14 @@ const firebase = require('firebase');
 
 const initialize = () => {
     const firebaseConfig = {
-        apiKey: "AIzaSyBg8FhwkH7inmMcpdyar32TBMNSGYjrRIE",
+        apiKey: "",
         authDomain: "football-card.firebaseapp.com",
-        databaseURL: "https://football-card.firebaseio.com",
-        projectId: "football-card",
-        storageBucket: "football-card.appspot.com",
-        messagingSenderId: "198548854173",
-        appId: "1:198548854173:web:7f0b494f57f1cf51e00b74",
-        measurementId: "G-3CVLRS7RVD"
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
       };
     
       // Initialize Firebase
